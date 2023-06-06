1146e91d8fa8eacf5d8349a72951c6b397b64818
Jsriprashant <jsriprashant567@gmail.com>
2023-06-06 09:28:06 +0530

    added milestone1, some changes made in servlets file

---
+++ h2h_milestone_jp/src/com/highradius/implementation/InvoiceDao.java
@@ -0,0 +1,11 @@
+package com.highradius.implementation;
+
+import com.highradius.model.Invoice;
+import java.util.List;
+
+public interface InvoiceDao {
+    List<Invoice> getInvoices();
+    void insertInvoice(Invoice invoice);
+    void updateInvoice(int id, Invoice updatedInvoice);
+    void deleteInvoice(int id);
+}
---
+++ h2h_milestone_jp/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author KIIT
+ *
+ */
+module h2h_milestone_1 {
+}
